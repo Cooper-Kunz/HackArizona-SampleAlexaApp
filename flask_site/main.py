53014b6a0c9071ece67d12eab1152e3f7e2f2c39
michaelhamm <michaelhamm@email.arizona.edu>
2017-01-14 13:14:29 -0700

    Added texting option

--- flask_site/main.py
+++ flask_site/main.py
@@ -4,9 +4,24 @@ import json
 import requests
 import time
 import unidecode
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 ask = Ask(app, "/reddit_reader")
+
+def send_text(outgoing, body):
+    # put your own credentials here
+    ACCOUNT_SID = "AC3fa7dba82f00ec577cd9da96e50ee0f2"
+    AUTH_TOKEN = "54ded7973a552d9fb6defccd3efac9ed"
+
+    #client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    print(outgoing, body)
+
+    #client.messages.create(
+    #    to=outgoing,
+    #    from_="+14807712950",
+    #    body=body,
+    #)
 
 def get_headlines():
     user_pass_dict = {'user': 'sampleaccountname',
@@ -21,7 +36,7 @@ def get_headlines():
     data = json.loads(html.content.decode('utf-8'))
     titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
     titles = '... '.join([i for i in titles])
-    return titles  
+    return titles
     
 @app.route('/')
 def homepage():
@@ -36,6 +51,7 @@ def start_skill():
 def share_headlines():
     headlines = get_headlines()
     headline_msg = 'Calling 911 and alerting your emergency contacts. Hold on...'
+    send_text("+16025617960", "Your emergency contact is wanting to notify you.")
     return statement(headline_msg)
 
 @ask.intent("NoIntent")
